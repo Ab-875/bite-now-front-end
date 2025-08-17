d14c51381fb7be8c3adcff676c2db6d3b8166009
Muhannadjz <muhannadzowayed05@gmail.com>
2025-08-17 14:39:25 +0300

    fixed some errors in the restaurant component

--- src/Components/RestaurantForm/RestaurantForm.jsx
+++ src/Components/RestaurantForm/RestaurantForm.jsx
@@ -2,32 +2,11 @@ import { useState } from "react";
 import axios from "axios";
 
 const RestaurantForm = ({ setFormIsShown }) => {
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [formData, setFormData] = useState({
-        name: '',
-        isOpen: false,  
-        rating: '',
-        menu: ""
-    });
-
-    const handleChange = (event) => {
-        const { name, type, value} = event.target;
-        setFormData({
-            ...formData,
-            [name]: type === 'boolean' 
-        });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setIsSubmitting(true);
-
-        console.log("Form submitted:", formData);
-        setIsSubmitting(false);
-    };
+    
 
     return (
-        <div>
+        <>
+                    <div>
             <h2>Add a Restaurant</h2>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="name">Restaurant Name</label>
@@ -71,23 +50,14 @@ const RestaurantForm = ({ setFormIsShown }) => {
                 {isSubmitting ? <p>Submitting...</p> : <button type="submit">Submit</button>}
             </form>
 
-            //Create a form that allows you to enter details that match the formData 
-            // Create a function that allows you to change form data 
-            //On submitting the form, run a function that handles the submitted data
-            // Map through the data of formData to display the details 
+            {/* //Create a form that allows you to enter details that match the formData  */}
+            {/* //Create a function that allows you to change form data  */}
+            {/* //On submitting the form, run a function that handles the submitted data */}
+            {/* //Map through the data of formData to display the details  */}
+            </div>
 
-<div>
-  {formData.name (
-    <div>
-      <h3>Submitted Restaurant Details:</h3>
-      <p><strong>Name:</strong> {formData.name}</p>
-      <p><strong>Is Open?</strong> {formData.isOpen}</p>
-      <p><strong>Rating:</strong> {formData.rating}</p>
-      <p><strong>Menu:</strong> {formData.menu}</p>
-    </div>
-  )}
-</div>
-
+        
+        </>
     );
 };
 
