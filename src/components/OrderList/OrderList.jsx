cc35ad0fcc69bd268efccf0b7b52ca5537f3f197
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-08-17 12:16:25 +0300

    added getAllOrders api call in orderList

--- src/components/OrderList/OrderList.jsx
+++ src/components/OrderList/OrderList.jsx
@@ -6,6 +6,14 @@ const OrderList = () => {
 
     const [order, setOrder] = useState()
 
+    const getAllOrders = async () => {
+        console.log(import.meta.env.VITE_BACKEND_URL)
+        const url = `${import.meta.env.VITE_BACKEND_URL}/order`
+        const response = await axios.get(url)
+        console.log(response)
+        setOrder(response.data)
+    }
+
     return (
         <>
 
