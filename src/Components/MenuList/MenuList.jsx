f7e66d4b565f3429265624e556bbc2aa35a61539
Muhannadjz <muhannadzowayed05@gmail.com>
2025-08-18 13:43:18 +0300

    menulist form is created and i added state for the items

--- src/Components/MenuList/MenuList.jsx
+++ src/Components/MenuList/MenuList.jsx
@@ -1,2 +1,19 @@
-import { useState } from "react";
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ClipLoader } from 'react-spinners';
+
+
+const MenuList = () => {
+  
+  
+    const [menuItems, setMenuItems] = useState([]);
+
+  const getAllMenuItems = async () => {
+    const url = `${import.meta.env.VITE_BACK_END_SERVER_URL}/menu`;
+
+const response = await axios.get(url);
+      setMenuItems(response.data);
+  };
+useEffect(() => {getAllMenuItems();},
+
+ []);
