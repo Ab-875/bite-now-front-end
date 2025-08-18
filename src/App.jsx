f9d7dffc5c8757c7b9f86326a4c4695efd8fb2fa
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-08-19 00:45:32 +0300

    should have finished app.jsx for now, just need the other components

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,76 @@
 import { jwtDecode } from "jwt-decode"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
-import { BrowserRouter as Router, Routes, Route } from "react-router"
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router"
 import LoginForm from "./components/Login/LoginForm"
 import SignUp from "./components/Signup/SignupForm"
 import LogoutButton from "./components/Login/LogoutButton"
-
 import OrderList from "./components/OrderList/OrderList"
+import Cart from "./components/Cart/Cart"
 
 const App = () => {
   const [token, setToken] = useState(localStorage.getItem('token'))
+  const [user, setUser] = useState(null)
+  const [cartItems, setCartItems] = useState([])
 
   function handleLogin(newToken) {
     setToken(newToken)
   }
 
-  if(token){
+  const handleLogout = (token) => {
+    localStorage.setItem("token", token)
+    setToken(null)
+    setUser(null)
+    setCartItems([])
+  }
+
+  if (token) {
     const decodedToken = jwtDecode(token)
+    setUser(decodedToken)
     console.log(decodedToken)
+  } else {
+    setUser(null)
+  }
+
+  if (!token) {
+    return (
+      <>
+        <Router>
+          <Routes>
+            <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </Router>
+      </>
+    )
   }
 
   return (
     <>
       <Router>
-        <div>
-        {token ? <LogoutButton onLogout={handleLogut} /> : null}
+        <LogoutButton onLogout={handleLogout} />
+        {user?.role === "customer"
+          ?
+          <Routes>
+            <Route path="/menu"
+              element={<MenuList token={token} />}
+              onAddToCart={(item) => { setCartItems([...cartItems, item]) }}
+            />
+            <Route path="*" element={<Navigate to="/menu" />} />
+          </Routes>
+          :
+          null
+        }
+
+        {user?.role === "owner"
+        ?
         <Routes>
-          <Route path="/order" element={<OrderList />}/>
-          <Route path="/login" element={<LoginForm onLogin={handleLogin} />}/>
-          <Route path="/signup" element={<SignUp />} />
+          <Route path="/orders" element={<OrderList token={token} />}/>
+          <Route path="*" element={<Navigate to="/order" />} />
         </Routes>
-        </div>
+        :
+        null
+        }
       </Router>
     </>
   )
