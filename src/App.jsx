14994aaa1f627ccd636ca24f4d63cfd263b69435
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-08-18 14:49:53 +0300

    added imports to app.jsx and the token state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { jwtDecode } from "jwt-decode"
+import { useState } from "react"
+
+import { BrowserRouter as Router, Routes, Route } from "react-router"
+import LoginForm from "./components/Login/LoginForm"
+import SignUp from "./components/Signup/SignupForm"
+import LogoutButton from "./components/Login/LogoutButton"
+
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'))
+
+  function handleLogin(newToken) {
+    setToken(newToken)
+  }
+
   return (
     <>
-    
+
     </>
   )
 }
