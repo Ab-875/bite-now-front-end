09e3e9036ff64fd0f5d04850c542f68f5d171ad9
Muhannadjz <muhannadzowayed05@gmail.com>
2025-08-19 09:36:56 +0300

    a function was deleted from the component

--- src/Components/MenuDelete/MenuDelete.jsx
+++ src/Components/MenuDelete/MenuDelete.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import axios from 'axios';
 import {deleteItem} from '../..//lib/api'
 
-const MenuDelete = ({petId,getAllItems}) => {
+const MenuDelete = ({ItemId,getAllItems}) => {
     const handleDelete = async () => {
             await deleteItem(ItemId)
-    }       getAllItems()
+    }      
 }
 
 
@@ -19,26 +19,3 @@ import {ClipLoader} from "react-spinners"
 
 import MenuDeleteButton from "../MenuList/MenuDeleteButton"
 
-
-const MenuList = () => {
-    const [items, setItems] = useState([])
-
-    const getAllItems = async () => {
-        // const url = ""
-        console.log(import.meta.env.VITE_BACKEND_URL)
-        const url = `${import.meta.env.VITE_BACKEND_URL}/items`
-
-        const response = await axios.get(url)
-        console.log(response)
-        setItems(response.data)
-    }
-    useEffect(() => {getAllItems()
-
-    },[])
-  return (
-    <button onClick={()=> handleDelete}>Delete</button>
-  );
-}
-
-
-export default ItemList
