cbda58e77760578af5db7fa3b8fd28a861e12684
Muhannadjz <muhannadzowayed05@gmail.com>
2025-08-21 07:56:51 +0300

    css folders added

--- src/Components/MenuList/MenuList.jsx
+++ src/Components/MenuList/MenuList.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import axios from "axios"
+import './MenuList.css'
 
 const MenuList = ({ token, role }) => {
   const [menus, setMenus] = useState([])
